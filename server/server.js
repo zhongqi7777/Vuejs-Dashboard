dcdbbee5cfdb828fde88e7380280ab3415991695
yanlibo2013 <yanlibo2010@163.com>
2019-10-30 14:39:04 +0800

    new file:   server/db/list.json

--- server/server.js
+++ server/server.js
@@ -1,14 +1,6 @@
-const bodyParser = require("body-parser");
-const jsonServer = require("json-server");
-
-const server = jsonServer.create();
-const router = jsonServer.router("./server/db/menu.json");
-
-server.use(jsonServer.defaults());
-server.use(bodyParser.json());
-
-server.use(router);
-
-server.listen(3000, () => {
-  console.log("The API server is running At http://localhost:3000");
-});
+module.exports = function() {
+  return {
+    menu: require("./db/menu.json"),
+    list: require("./db/list.json")
+  };
+};
