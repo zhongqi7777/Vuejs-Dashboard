4f0c736718b541cf971677bde2a200d3dd6caf18
yanlibo2013 <yanlibo2010@163.com>
2019-10-31 10:32:46 +0800

    new file:   server/db/routes.json

--- server/index.js
+++ server/index.js
@@ -1,22 +1,8 @@
-const fs = require("fs");
-const bodyParser = require("body-parser");
-const jsonServer = require("json-server");
-// const jwt = require('jsonwebtoken')
-// var cors = require('cors');
-
-const server = jsonServer.create();
-const router = jsonServer.router({
-  menu: require("./db/menu.json"),
-  userlist: require("./db/userlist.json"),
-  adduser: require("./db/userlist.json")
-});
-
-server.use(jsonServer.defaults());
-//server.use(bodyParser.urlencoded({extended: true}))
-server.use(bodyParser.json());
-
-server.use(router);
-
-server.listen(7000, () => {
-  console.log("The API server is running At http://localhost:7000");
-});
+let userlist = require("./data/userlist.json");
+module.exports = function() {
+  return {
+    menu: require("./data/menu.json"),
+    userlist: userlist,
+    adduser: userlist
+  };
+};
--- src/components/layout/Sidebar/index.vue
+++ src/components/layout/Sidebar/index.vue
@@ -57,9 +57,9 @@ export default {
     // ...mapActions("permission",["getMenuData"]),
     initData() {
       getMenu().then(res => {
-        console.log(" getMenu().then(res => {", res.data.menu);
+        console.log(" getMenu().then(res => {", res.data);
         // this.getMenuData(res.data.menu);
-        this.$store.dispatch('permission/getMenuData',res.data.menu)
+        this.$store.dispatch('permission/getMenuData',res.data)
       });
     }
   },
