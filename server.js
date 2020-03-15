c00b85aff13059e5fc25cfaa359cff81e927db0a
unknown <yanlibo2010@163.com>
2020-03-15 23:16:02 +0800

    modified:   README.md         modified:   package.json

--- server.js
+++ server.js
@@ -10,26 +10,26 @@ const port = process.env.PORT || 80;
 app.listen(port);
 
 //jsonServer
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('./server/db/db.json');
-// const middlewares = jsonServer.defaults();
-// // const port = process.env.PORT || 3000;
-// const serverport = 3000;
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./server/db/db.json');
+const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3000;
+const serverport = 3000;
 
-// server.use(middlewares);
+server.use(middlewares);
 
-// // Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     "/del/user/:id": "/userlist/:id",
-//     "/modify/user/:id": "/userlist/:id",
-//     "/add/user": "/userlist",
-//     "/del/flow/:id": "/flowlist/:id",
-//     "/modify/flow/:id": "/flowlist/:id",
-//     "/add/flow": "/flowlist",
-//     "/rtc/steps/settings/source": "/source",
-//     "/rtc/steps/settings/sink": "/sink"
-// }))
-// server.use("/api", router);
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    "/del/user/:id": "/userlist/:id",
+    "/modify/user/:id": "/userlist/:id",
+    "/add/user": "/userlist",
+    "/del/flow/:id": "/flowlist/:id",
+    "/modify/flow/:id": "/flowlist/:id",
+    "/add/flow": "/flowlist",
+    "/rtc/steps/settings/source": "/source",
+    "/rtc/steps/settings/sink": "/sink"
+}))
+server.use("/api", router);
 
-// server.listen(serverport);
+server.listen(serverport);
