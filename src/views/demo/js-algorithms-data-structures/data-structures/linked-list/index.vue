d588e3b96964604d689d76eb7b296570d084fc9c
zhongqi7777 <yanlibo@300.cn>
2020-08-27 20:40:22 +0800

    modified:   src/views/demo/js-algorithms-data-structures/data-structures/linked-list/index.vue

--- src/views/demo/js-algorithms-data-structures/data-structures/linked-list/index.vue
+++ src/views/demo/js-algorithms-data-structures/data-structures/linked-list/index.vue
@@ -28,7 +28,44 @@ export default {
   computed: {
     //...mapState([""])
   },
-  mounted() {},
+  mounted() {
+    const linkedList = new LinkedList();
+
+    // linkedList.append(1);
+    // linkedList.append(2);
+
+    //linkedList.delete(JSON.stringify({ name: "北京" }));
+
+    //const deletedNode1 = linkedList.deleteTail();
+    const deletedNode2 = linkedList.deleteHead();
+
+    // let result = linkedList.toString();
+
+    //console.log("deletedNode1", deletedNode1);
+    // console.log("deletedNode2", deletedNode2);
+
+    // console.log("result", result);
+
+    // linkedList.append(JSON.stringify({ name: "北京" }));
+    // linkedList.append(JSON.stringify({ name: "上海" }));
+    // linkedList.prepend(JSON.stringify({ name: "深圳" }));
+    linkedList
+      .append({ name: "北京" })
+      .append({ name: "上海" })
+      .append({ name: "深圳" })
+      .reverse();
+
+    // console.log("linkedList", linkedList);
+
+    // const nodeStringifier = value => `${value.name}`;
+    // let resuslt = linkedList.toString(nodeStringifier);
+
+    // console.log("result", resuslt);
+
+    const node = linkedList.find({ callback: value => value.name === "上海" });
+
+    console.log("node", node);
+  },
   beforeCreate() {},
   created() {},
   beforeMount() {},
