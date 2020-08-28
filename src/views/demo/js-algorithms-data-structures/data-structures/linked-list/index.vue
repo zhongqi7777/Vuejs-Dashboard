75f90be6c9898da8f2bd9e77d95d66011fef907e
zhongqi7777 <yanlibo@300.cn>
2020-08-28 13:28:24 +0800

    modified:   src/views/demo/js-algorithms-data-structures/data-structures/linked-list/index.vue

--- src/views/demo/js-algorithms-data-structures/data-structures/linked-list/index.vue
+++ src/views/demo/js-algorithms-data-structures/data-structures/linked-list/index.vue
@@ -9,6 +9,7 @@
 
 <script>
 import LinkedList from "./lib/LinkedList"; //单向链表
+import { json } from "body-parser";
 
 export default {
   watch: {
@@ -37,7 +38,7 @@ export default {
     //linkedList.delete(JSON.stringify({ name: "北京" }));
 
     //const deletedNode1 = linkedList.deleteTail();
-    const deletedNode2 = linkedList.deleteHead();
+    // const deletedNode2 = linkedList.deleteHead();
 
     // let result = linkedList.toString();
 
@@ -49,11 +50,11 @@ export default {
     // linkedList.append(JSON.stringify({ name: "北京" }));
     // linkedList.append(JSON.stringify({ name: "上海" }));
     // linkedList.prepend(JSON.stringify({ name: "深圳" }));
-    linkedList
-      .append({ name: "北京" })
-      .append({ name: "上海" })
-      .append({ name: "深圳" })
-      .reverse();
+    // linkedList
+    //   .append({ name: "北京" })
+    //   .append({ name: "上海" })
+    //   .append({ name: "深圳" })
+    //   .reverse();
 
     // console.log("linkedList", linkedList);
 
@@ -62,9 +63,64 @@ export default {
 
     // console.log("result", resuslt);
 
-    const node = linkedList.find({ callback: value => value.name === "上海" });
+    // linkedList
+    //   .append(JSON.stringify({ name: "北京" }))
+    //   .append(JSON.stringify({ name: "上海" }))
+    //   .append(JSON.stringify({ name: "深圳" }));
 
-    console.log("node", node);
+    // const node = linkedList.find({
+    //   callback: value => {
+    //     let node = JSON.parse(value);
+    //     console.log(value);
+    //     console.log(node);
+    //     value === JSON.stringify({ name: "北京" });
+    //   }
+    // });
+
+    //linkedList.delete(JSON.stringify({ name: "北京" }));
+
+    // linkedList
+    //   .append({ name: "北京" })
+    //   .append({ name: "上海" })
+    //   .append({ name: "深圳" });
+    // let node = linkedList.find({ name: "深圳" });
+    // console.log("node", node);
+
+    // const comparatorFunction = (a, b) => {
+    //   if (a.customValue === b.customValue) {
+    //     return 0;
+    //   }
+
+    //   return a.customValue < b.customValue ? -1 : 1;
+    // };
+
+    // const linkedList = new LinkedList(comparatorFunction);
+
+    // linkedList.append({ value: 1, customValue: { name: "北京" } });
+    // // .append({ value: 2, customValue: "test2" })
+    // // .append({ value: 3, customValue: "test3" });
+
+    // const node = linkedList.find({
+    //   value: { value: 1, customValue: { name: "北京" } }
+    // });
+
+    // console.log("node", node);
+
+    // console.log("linkedList", linkedList);
+
+    // linkedList
+    //   .append({ value: 1, key: "test1" })
+    //   .append({ value: 2, key: "test2" })
+    //   .append({ value: 3, key: "test3" });
+
+    // const node = linkedList.find({ callback: value => value.key === "test2" });
+
+    // linkedList.delete({ value: 1, key: "test1" });
+
+    //linkedList.fromArray([1, 1, 2, 3, 3, 3, 4, 5]);
+
+    // console.log(node);
+    //console.log(linkedList);
   },
   beforeCreate() {},
   created() {},
